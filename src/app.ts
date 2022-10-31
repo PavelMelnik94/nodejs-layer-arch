7a2413848db913f336bbe61c272cacc07ea32434
Pavel Melnik <p.melnik@intelico.su>
2022-10-31 12:57:00 +0300

    added logger

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
 import express, { Express } from "express";
 import userRouter from "./users/users.js";
 import { Server } from "http";
+import { LoggerService } from "./logger/logger.service.js";
 
 export class App {
     app: Express;
     server: Server;
     port: number;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
-    useRoutes() {
+    private useRoutes() {
         this.app.use("/users", userRouter);
     }
 
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Server running at port ${this.port}`);
+        this.logger.log(`Server running at port ${this.port}`);
     }
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,27 @@
+import { Logger } from 'tslog';
+
+export class LoggerService {
+    private logger: Logger;
+
+    constructor() {
+        this.logger = new Logger({
+            displayInstanceName: false,
+            displayFunctionName: false,
+            displayLoggerName: false,
+            displayFilePath: 'hidden',
+        });
+    }
+
+
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    };
+
+    error(...args: unknown[]) {
+        this.logger.error(...args);
+    };
+
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    };
+}
